eb499aacd97e914624c25d3f041b8e44103937a4
Rohan Dhimal <drowhann@gmail.com>
2023-07-03 18:10:59 +0545

    feat: add code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  content: {
+    highlight: {
+      // Theme used in all color schemes.
+      theme: 'github-light',
+      preload: ['diff', 'ts', 'js', 'css', 'java', 'groovy', 'sql', 'xml', 'json'],
+    },
+  },
 })
