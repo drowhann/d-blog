e3334e1a6015b46b8f58ce1559a746c40a82b0c2
Rohan Dhimal <drowhann@gmail.com>
2023-11-17 09:24:23 +0545

    chore: update font size and navbar links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="p-2">
         <section class="flex justify-between items-center">
-            <NuxtLink to="/" class="text-lg font-bold">drowhann.</NuxtLink>
+            <NuxtLink to="/" class="text-2xl font-bold">drowhann.</NuxtLink>
             <div class="flex gap-4 items-center">
-                <NuxtLink to="/blog" class="text-sm font-bold uppercase">Blogs</NuxtLink>
-                <NuxtLink to="/project" class="text-sm font-bold uppercase">Projects</NuxtLink>
+                <NuxtLink to="/blog" class="font-bold uppercase">Blogs</NuxtLink>
+                <NuxtLink to="/project" class="font-bold uppercase">Projects</NuxtLink>
                 <div class="w-5 h-5 dark:bg-gray-100 bg-gray-900 rounded-full cursor-pointer"
                     @click="$colormode.toggleColorMode()">
                 </div>
